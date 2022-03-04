92c33f270d62bc7169685ac4b30b1f79b936c73a
MaheshMishra910 <46555254+MaheshMishra910@users.noreply.github.com>
2022-03-04 14:28:04 +0545

    Project upload first time

---
+++ flowerShop/mainApp/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
+
+# Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "index-2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Product.objects.all().order_by("-id")[:8]
+        return context
+    
+    
+
+
+class ContactView(TemplateView):
+    template_name = "contact-us.html"
+    
+
+class AboutView(TemplateView):
+    template_name = "about-us.html"
+
+
+class ShopView(TemplateView):
+    template_name = "shop.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['allcategories'] = Product.objects.all()
+        return context
+
+
+class ProductDetailView(TemplateView):
+    template_name = "product-details-2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get(slug=url_slug)
+        product.view_count += 1
+        product.save()
+        context['product'] = product
+        return context
+
+class AddToCartView(TemplateView):
+    template_name = "addtocart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #for get product id from requested url
+        product_id = self.kwargs['pro_id']
+        #for get product
+        product_obj = Product.objects.get(id=product_id)
+        #for cart ma aagadi xavane check garne
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(
+                product=product_obj)
+            #for items already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.selling_prince
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_prince
+                cart_obj.save()
+            #for new item id added in cart
+            else:
+                cartproduct = CartProduct.objects.create(
+                    cart=cart_obj, product=product_obj, rate=product_obj.selling_prince, quantity=1, subtotal=product_obj.selling_prince
+                )
+                cart_obj.total += product_obj.selling_prince
+                cart_obj.save()
+                
+
+
+
+        else:
+            cart_obj = Cart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = CartProduct.objects.create(
+                    cart=cart_obj, product=product_obj, rate=product_obj.selling_prince, quantity=1, subtotal=product_obj.selling_prince
+                )
+            cart_obj.total += product_obj.selling_prince
+            cart_obj.save()
+
+
+        
+        return context
+
+class MyCartView(TemplateView):
+    template_name = "cart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+
+
+
+
+
+class BlogView(TemplateView):
+    template_name = "blog.html"
