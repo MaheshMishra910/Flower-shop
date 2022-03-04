f320fb11a6499c1167f44b10ae03ddc933086bfe
MaheshMishra910 <46555254+MaheshMishra910@users.noreply.github.com>
2022-03-04 22:13:16 +0545

    cart update

--- flowerShop/mainApp/views.py
+++ flowerShop/mainApp/views.py
@@ -92,6 +92,16 @@ class AddToCartView(TemplateView):
         
         return context
 
+class EmptyCartView(View):
+    def get(self, request, *args, **kwargs):
+        cart_id = request.session.get("cart_id", None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+            cart.cartproduct_set.all().delete()
+            cart.total = 0
+            cart.save()
+        return redirect("mainApp:mycart")
+
 class ManageCartView(View):
     def get(self, request, *args, **kwargs):
         cp_id = self.kwargs["cp_id"]
